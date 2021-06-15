5c98362ab99a7848686ec74917d50576b22a414d
kaustavbose <42936612+kaustavbose@users.noreply.github.com>
2021-06-15 21:21:52 +0530

    Update Navbar.js

--- views/components/Navbar.js
+++ views/components/Navbar.js
@@ -23,7 +23,12 @@ const Navbar = {
       <a id="expenses-link" href="#expenses">Expenses</a>
     </li>
     <li class="spacer" />
-    const isAuthenticated = await window.auth0Client.isAuthenticated();
+    <li id="log-out" style="display: ${isAuthenticated ? "block" : "none"}">
+      <a href="#"> Logout<a>
+    <li>
+    <li id="log-in" style="display: ${isAuthenticated ? "none" : "block"}">
+      <a href="#"> Login<a>
+    <li>
     <li class="profile">
       <img src="${window.user.picture}" />
     </li>
